03b57ed2cb418e45b427d2ab2720ee23c2e27d76
jonathasmatos <jonathasvirtual@gmail.com>
2023-10-18 11:45:03 -0300

    Status

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,39 @@
     <Fieldset legend="Gerenciamento de Agenda">
       <Toolbar>
         <template #start>
-          <Button label="Novo" />
+          <Button
+              label="Novo"
+              icon="pi pi-plus"
+              class="mr-2"
+              @click="showCreate()"
+            />
         </template>
       </Toolbar>
-      <DataTable :value="list">
+      <DataTable 
+          ref="dt"
+          dataKey="id"
+          class="p-datatable-sm"
+          :value="list"
+          :paginator="true"
+          :rows="10"
+          :rowsPerPageOptions="[10, 20, 50]"
+          :filters="filters"
+          paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+          currentPageReportTemplate="Exibindo de {first} a {last} no total de {totalRecords} registros."
+          :loading="loading"
+      >
+        <template #empty>
+            <div class="p-text-center">Nenhum resultado encontrado...</div>
+          </template>
         <Column header="Nome" field="name" :sortable="true">
-          <!-- <template #body="slotProps">
+          <template #body="slotProps">
               {{ slotProps.data.name}}
-            </template> -->
+            </template>
         </Column>
         <Column header="Contato" field="contact" :sortable="true">
-          <!-- <template #body="slotProps">
-              {{ slotProps.data.name}}
-            </template> -->
+          <template #body="slotProps">
+              {{ slotProps.data.contact}}
+            </template>
         </Column>
         <Column field="status" header="Status" ref="status">
             <template #body="slotProps">
@@ -56,17 +76,28 @@ export default {
       obj: new Agenda(),
       service: new AgendaService(),
       list: [],
+      filters: {},
+      loading: false
     };
   },
-  created() {},
   mounted() {
     this.findAll();
+    this.changeStatus()
   },
   methods: {
+    showCreate() {
+      this.obj = new Agenda();
+      this.$store.state.views.agenda.dialogForm = true;
+    },
     findAll() {
       this.service.findAll().then((data) => {
         this.list = data;
         console.log(this.list);
+      });
+    },
+    changeStatus(data) {
+      this.service.changeStatus(data.id).then(() => {
+        this.findAll();
       });
     },
   },
@@ -74,11 +105,11 @@ export default {
 </script>
 
 <style scoped>
-status-true {
+badge status- {
     background: #c8e6c9;
     color: #256029;
   }
-  status-false {
+  badge status- {
     background: #ffcdd2;
     color: #c63737;
   }
--- src/service/agenda_service.js
+++ src/service/agenda_service.js
@@ -9,6 +9,9 @@ export default class AgendaService {
   findById(id) {
     return axios.get(`${"http://localhost:8080/agenda"}/${id}`);
   }
+  findByStatus(status) {
+    return this.execute("GET", `/status/${status}`).then((res) => res.data);
+  }
 
   create(obj) {
     return axios.post("http://localhost:8080/agenda", obj);
@@ -16,6 +19,9 @@ export default class AgendaService {
 
   update(obj) {
     return axios.put("http://localhost:8080/agenda", obj);
+  }
+  changeStatus(id) {
+    return axios.put(`${"http://localhost:8080/agenda"}/${id}`);
   }
 
   delete(id) {
